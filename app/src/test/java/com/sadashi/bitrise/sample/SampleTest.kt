5b5213af0b6ab94fc9daea06b3c11dc3faa6e453
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-06-30 11:05:25 +0900

    Modify test code.

--- app/src/test/java/com/sadashi/bitrise/sample/SampleTest.kt
+++ app/src/test/java/com/sadashi/bitrise/sample/SampleTest.kt
@@ -14,17 +14,17 @@ class SampleTest {
         assertTrue(result)
     }
 
-    @Test
-    fun testCheckArgument_arg1_is_null() {
-        val arg2 = "2"
-        val result = Sample().checkArgument(null, arg2)
-        assertFalse(result)
-    }
-
-    @Test
-    fun testCheckArgument_arg2_is_null() {
-        val arg1 = "1"
-        val result = Sample().checkArgument(arg1, null)
-        assertFalse(result)
-    }
+//    @Test
+//    fun testCheckArgument_arg1_is_null() {
+//        val arg2 = "2"
+//        val result = Sample().checkArgument(null, arg2)
+//        assertFalse(result)
+//    }
+//
+//    @Test
+//    fun testCheckArgument_arg2_is_null() {
+//        val arg1 = "1"
+//        val result = Sample().checkArgument(arg1, null)
+//        assertFalse(result)
+//    }
 }
