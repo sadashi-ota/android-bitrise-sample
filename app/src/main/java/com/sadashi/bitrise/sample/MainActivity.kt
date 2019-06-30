3ce4a706dd15545a7be7df30c33d85d1aa13cc49
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-06-30 10:51:22 +0900

    Long log tag.

--- app/src/main/java/com/sadashi/bitrise/sample/MainActivity.kt
+++ app/src/main/java/com/sadashi/bitrise/sample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sadashi.bitrise.sample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("abcdefghijklmnopqrstuvwxyz", "")
     }
 }
